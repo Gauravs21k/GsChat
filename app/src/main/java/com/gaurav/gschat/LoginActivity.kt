481d02273f316d6322843cfec2a48800ec6f833a
Gaurav Singh <gauravs21k@gmail.com>
2022-03-09 01:30:13 +0530

    signup button is handlled

--- app/src/main/java/com/gaurav/gschat/LoginActivity.kt
+++ app/src/main/java/com/gaurav/gschat/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.gaurav.gschat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var  btnSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnSignUp = findViewById(R.id.button_sign_up)
+        btnSignUp.setOnClickListener {
+            val intent: Intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
