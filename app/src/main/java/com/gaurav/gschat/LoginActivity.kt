29375459eb81cd4dc7c61314d33ac50b67e579ca
Gaurav Singh <gauravs21k@gmail.com>
2022-03-09 14:50:49 +0530

    Firebase setup done, authentication functionalty implemented

--- app/src/main/java/com/gaurav/gschat/LoginActivity.kt
+++ app/src/main/java/com/gaurav/gschat/LoginActivity.kt
@@ -1,21 +1,55 @@
 package com.gaurav.gschat
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var  btnSignUp: Button
+    lateinit var btnLogin: Button
+    lateinit var editUserName: EditText
+    lateinit var editPassword: EditText
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
         btnSignUp = findViewById(R.id.button_sign_up)
+        editUserName = findViewById(R.id.login_username)
+        editPassword = findViewById(R.id.login_password)
+
         btnSignUp.setOnClickListener {
-            val intent: Intent = Intent(this, SignUpActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        btnLogin = findViewById(R.id.button_login)
+
+        btnLogin.setOnClickListener {
+            val email = editUserName.text.toString()
+            val password = editPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@LoginActivity, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/gaurav/gschat/SignUpActivity.kt
+++ app/src/main/java/com/gaurav/gschat/SignUpActivity.kt
@@ -1,11 +1,47 @@
 package com.gaurav.gschat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var  btnSignUp: Button
+    lateinit var  editName: EditText
+    lateinit var editEmail: EditText
+    lateinit var editPassword: EditText
+    lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+        btnSignUp = findViewById(R.id.button_sign_up)
+        editName = findViewById(R.id.name)
+        editEmail = findViewById(R.id.signup_username)
+        editPassword = findViewById(R.id.signup_password)
+
+        btnSignUp.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+            signUp(email, password)
+        }
+    }
+
+    private fun signUp(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, jump to login page
+                    val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@SignUpActivity, "Sign up failed",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
